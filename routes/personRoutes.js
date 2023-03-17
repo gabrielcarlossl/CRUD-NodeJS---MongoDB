6e480986d27737d0e64cf3db05b0c8992db5cf08
gabrielcarlossl <gabrielcarlossleite@gmail.com>
2023-03-16 21:55:28 -0300

    feat: add routes to patch, delete

--- routes/personRoutes.js
+++ routes/personRoutes.js
@@ -16,15 +16,19 @@ router.post('/', async (req, res) => {
 
   if (!name) {
     res.status(422).json({ error: 'You must set a name!' })
+    return
   }
   if (!birthDate) {
     res.status(422).json({ error: 'You must set a birtdate!' })
+    return
   }
   if (!salary) {
     res.status(422).json({ error: 'You must set a salary!' })
+    return
   }
   if (!approved) {
     res.status(422).json({ error: 'You must set if is approved!' })
+    return
   }
 
   const person = {
@@ -62,11 +66,70 @@ router.get('/:id', async (req, res) => {
 
     if (!person) {
       return res.status(404).json({ error: 'Person not found!' })
+      
     }
 
     res.status(200).json(person)
   } catch (error) {
-    res.status(404).json({ error: `The person's ID = ${req.params.id} was not found!` })
+    res
+      .status(404)
+      .json({ error: `The person's ID = ${req.params.id} was not found!` })
+  }
+})
+
+// Update - atualização de dados (PUT, PATCH) PUT é obrigado trocar todos os dados
+// PATCH é possível trocar apenas um dos dados
+
+router.patch('/:id', async (req, res) => {
+  const id = req.params.id
+
+  try {
+    const person = await Person.findById(id)
+
+    if(!person){
+      return res.status(404).json({message: 'Person not found!'})
+    }
+
+    if(req.body.name){
+      person.name = req.body.name
+    }
+
+    if(req.body.birthDate){
+      person.birthDate = new Date(req.body.birthDate)
+    }
+
+    if(req.body.salary){
+      person.salary = req.body.salary
+    }
+
+    if(req.body.approved !== undefined){
+      person.approved = req.body.approved
+    }
+
+    const updatedPerson = await person.save()
+    res.status(200).json({ message: 'Person updated successfully!' })
+
+    res.status(200).json(updatedPerson)
+  } catch (error) {
+    res
+      .status(404)
+      .json({ error: `The person's ID = ${id} was not found!` })
+  }
+})
+
+router.delete('/:id', async (req, res) => {
+  try {
+    const person = await Person.findByIdAndDelete(req.params.id)
+
+    if (!person) {
+      return res.status(404).json({ error: 'Person not found!' })
+    }
+
+    res.status(200).json({ message: 'Person deleted successfully!' })
+  } catch (error) {
+    res
+      .status(404)
+      .json({ error: `The person's ID = ${req.params.id} was not found!` })
   }
 })
 
